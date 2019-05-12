d7ebe8453c19df12a47e0ab8f32f2aafd34dd88a
Syed Mahfuzul Mazid <mahfuzt786@yahoo.co.in>
2019-05-13 02:34:47 +0530

    inventory popup added

--- assets/js/inventory.js
+++ assets/js/inventory.js
@@ -39,7 +39,7 @@ function addInventoryDetails() {
                 inventory_details += '<div class="control-group"><label class="control-label">Registration #</label><div class="controls readonly">'+data.registration_number+'</div></div>';
                 inventory_details += '<div class="control-group"><label class="control-label">Note</label><div class="controls readonly">'+data.note+'</div></div>';
                 inventory_details += '<div class="control-group"><label class="control-label">Count</label><div class="controls readonly">'+(data.count== null || data.count == 0 ? 'Sold Out' : data.count)+'</div></div>';
-                inventory_details += '<div class="control-group text-center"><img style="max-width:200px; margin: 10px;" src="images/'+ data.image_url_1+'"/><img style="max-width:200px; margin: 10px;" src="images/'+ data.image_url_2+'"/></div>';
+                inventory_details += '<div class="control-group text-center"><img style="max-width:200px; margin: 10px;" src="images/'+ data.image_url+'"/></div>';
                 inventory_details += '<div class="form-group text-center"><button type="button" class="btn btn-default" onclick="soldOut('+id+', \''+data.model_name+'\')">Sold Out</button></div>';
                 $('.modal-title').html(data.model_name);
                 $('.modal-body').html(inventory_details);
@@ -56,6 +56,57 @@ function addInventoryDetails() {
         }        
     
     } );
+}
+
+function soldOut(id, model_name) {
+
+    $.ajax({
+        'url':'services/sold_out_inventory.php',
+        method: "POST",
+        data: {'id':id},
+        success: function(data) {
+            switch(data) {
+                case 'success':
+                        var msg = '<div class="alert alert-info text-center">';
+                            msg += '<strong>Success!</strong> Model ' + model_name + ' Sold Out';
+                            msg += '</div>';
+                            addSoldOutNotification(id);
+                            alert("Data updated successfully");
+                        break;
+                case 'error':
+                        var msg = '<div class="alert alert-danger text-center">';
+                            msg += '<strong>Failed!</strong> Please retry.';
+                            msg += '</div>';
+                            alert("Please retry.");
+                        break;
+            }
+
+            loadInventories();
+            addInventoryDetails();
+            
+        }
+    });
+}
+
+function addSoldOutNotification(model_id) {
+    $.ajax({
+        'url':'services/notification.php',
+        method: "POST",
+        data: {'model_id' : model_id, 'action' : 'add'},
+        success: function(data) {
+            
+        }
+    });
+}
+
+function sendNotification(model_id) {
+    $.ajax({
+        'url':'services/notification.php',
+        method: "POST",
+        data: {'model_id' : model_id, 'action' : 'update'},
+        success: function(data) {
+        }
+    });
 }
 
 loadInventories();
