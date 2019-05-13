73943f19a3eda40172fec96e947af040f6a81589
Syed Mahfuzul Mazid <mahfuzt786@yahoo.co.in>
2019-05-13 21:47:52 +0530

    model image modified.

--- services/add_model.php
+++ services/add_model.php
@@ -4,44 +4,14 @@ require '../includes/util.php';
 
 $model = new Model();
 
-if($image1) {
-    $image1 = explode('.', $_FILES['image_file1']['name']);
-    $image1 = $image1[0] . "_" . time() . "." . end($image1);
-
-    $allowedExts = array("gif", "jpeg", "jpg", "png", "bmp");
-    $extension = end($image1);
-    $extension=strtolower($extension);
-
-    if ((($_FILES["image_file1"]["type"] == "image/gif")
-        || ($_FILES["image_file1"]["type"] == "image/jpeg")
-        || ($_FILES["image_file1"]["type"] == "image/jpg")
-        || ($_FILES["image_file1"]["type"] == "image/pjpeg")
-        || ($_FILES["image_file1"]["type"] == "image/x-png")
-        || ($_FILES["image_file1"]["type"] == "image/png")
-        || ($_FILES["image_file1"]["type"] == "image/bmp"))
-        && ($_FILES["image_file1"]["size"] < 1500000)
-        && in_array($extension, $allowedExts))
-    {
-        if ( $_FILES['image_file1']['error'] > 0 ){
-            echo 'Error: ' . $_FILES['image_file1']['error'] . '<br>';
-        }
-        else {
-            move_uploaded_file($_FILES['image_file1']['tmp_name'], '../images/' . $image1);
-        }
-    }
-    else
-    {
-        echo 'Invalid upload file.. Should be JPG,PNG,GIF,BMP and max 1500KB';
-    }
-}
-
-$manufacturer_id = cleanValue(getPost("manufacturer_id"));
-$name = cleanValue(getPost("model-name"));
-$color = cleanValue(getPost("model-color"));
-$year = cleanValue(getPost("model-year"));
-$registration_number = cleanValue(getPost("model-reg-no"));
-$note = cleanValue(getPost("model-note"));
-$count = cleanValue(getPost("model-count"));
+$manufacturer_id        = cleanValue(getPost("manufacturer_id"));
+$name                   = cleanValue(getPost("model-name"));
+$color                  = cleanValue(getPost("model-color"));
+$year                   = cleanValue(getPost("model-year"));
+$registration_number    = cleanValue(getPost("model-reg-no"));
+$note                   = cleanValue(getPost("model-note"));
+$count                  = cleanValue(getPost("model-count"));
+$imageFile              = getPost("image_file1");
 
 if( $manufacturer_id === 'null' )
 {
@@ -72,16 +42,46 @@ elseif( $count == 'null' )
     echo "Please enter model count";
 }
 else {
-    $data = [
-            "manufacturer_id" => cleanValue(getPost("manufacturer_id")),
-            "name" => cleanValue(getPost("model-name")),
-            "color" => cleanValue(getPost("model-color")),
-            "year" => cleanValue(getPost("model-year")),
-            "registration_number" => cleanValue(getPost("model-reg-no")),
-            "note" => cleanValue(getPost("model-note")),
-            "count" => cleanValue(getPost("model-count")),
-            "image_url" => $image1
-        ];
+    $image1 = explode('.', $_FILES['image_file1']['name']);
+    $extension = end($image1);
 
-    echo $model->insert($data);
+    $image1 = $image1[0] . "_" . time() . "." . end($image1);
+
+    $allowedExts = array("gif", "jpeg", "jpg", "png", "bmp");
+    $extension = strtolower($extension);
+
+    if ((($_FILES["image_file1"]["type"] == "image/gif")
+        || ($_FILES["image_file1"]["type"] == "image/jpeg")
+        || ($_FILES["image_file1"]["type"] == "image/jpg")
+        || ($_FILES["image_file1"]["type"] == "image/pjpeg")
+        || ($_FILES["image_file1"]["type"] == "image/x-png")
+        || ($_FILES["image_file1"]["type"] == "image/png")
+        || ($_FILES["image_file1"]["type"] == "image/bmp"))
+        && ($_FILES["image_file1"]["size"] < 2500000)
+        && in_array($extension, $allowedExts))
+    {
+        if ( $_FILES['image_file1']['error'] > 0 ){
+            echo 'Error: ' . $_FILES['image_file1']['error'] . '<br>';
+        }
+        else {
+            move_uploaded_file($_FILES['image_file1']['tmp_name'], '../images/' . $image1);
+
+            $data = [
+                "manufacturer_id" => cleanValue(getPost("manufacturer_id")),
+                "name" => cleanValue(getPost("model-name")),
+                "color" => cleanValue(getPost("model-color")),
+                "year" => cleanValue(getPost("model-year")),
+                "registration_number" => cleanValue(getPost("model-reg-no")),
+                "note" => cleanValue(getPost("model-note")),
+                "count" => cleanValue(getPost("model-count")),
+                "image_url" => $image1
+            ];
+
+            echo $model->insert($data);
+        }
+    }
+    else
+    {
+        echo 'Invalid upload file.. Should be JPG,PNG,GIF,BMP and max 2500KB';
+    }
 }
